86446759cdbc79d81a318b95571815090c08c60c
djdlzn2fjs <djdlzn2fjs@naver.com>
2020-11-09 14:59:51 +0900

    템플릿 설정 및 테스트

--- board/urls.py
+++ board/urls.py
@@ -3,5 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # url 패턴, 설정파일명.함수명
     path('', views.index),
+
+    path('list/', views.list),
+    path('view/', views.view),
+    path('write/', views.write),
+    path('update/', views.update),
+    path('delete/', views.delete),
+
 ]
--- board/views.py
+++ board/views.py
@@ -3,4 +3,24 @@ from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('Hello, Django Board!!')
+    return render(request, 'index.html')
+
+
+def list(request):
+    return render(request, 'board/list.html')
+
+
+def view(request):
+    return render(request, 'board/view.html')
+
+
+def write(request):
+    return render(request, 'board/write.html')
+
+
+def update(request):
+    return render(request, 'board/update.html')
+
+
+def delete(request):
+    return render(request, 'board/delete.html')
