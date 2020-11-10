5db78da83dd19e1265718d649833d50d653c1901
djdlzn2fjs <djdlzn2fjs@naver.com>
2020-11-10 10:55:50 +0900

    django model 적용

--- board/models.py
+++ board/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from django.utils import timezone
+
+
+class Board(models.Model):
+
+    title = models.CharField(max_length=100)
+    userid = models.CharField(max_length=18)
+    regdate = models.DateTimeField(default=timezone.localtime)
+    views = models.IntegerField(default=0)
+    thumbup = models.IntegerField(default=0)
+    contents = models.TextField()
+
+
